0d0de2fe6defaeaed7f31a8a5f91e9b3d0866439
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-01-30 12:46:40 +0000

    Load Google Searches

--- src/Search/LoadResults.java
+++ src/Search/LoadResults.java
@@ -1,7 +1,34 @@
 package Search;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LoadResults {
 	
-	//testing out Git
+	public static void main(String[] args) throws Exception {
+
+	    String key="AIzaSyAfj4MY2Z6Oqur_DWdI83coqtrgrqQhDW8";
+	    String qry="MrinalSeth";
+	    URL url = new URL(
+	            "https://www.googleapis.com/customsearch/v1?key="+key+ "&cx=013036536707430787589:_pqjad5hr1a&q="+ qry + "&alt=json");
+	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	    conn.setRequestMethod("GET");
+	    conn.setRequestProperty("Accept", "application/json");
+	    BufferedReader br = new BufferedReader(new InputStreamReader(
+	            (conn.getInputStream())));
+
+	    String output;
+	    System.out.println("Output from Server .... \n");
+	    while ((output = br.readLine()) != null) {
+
+	        if(output.contains("\"link\": \"")){                
+	            String link=output.substring(output.indexOf("\"link\": \"")+("\"link\": \"").length(), output.indexOf("\","));
+	            System.out.println(link);       //Will print the google search links
+	        }     
+	    }
+	    conn.disconnect();                              
+	}
 
 }
